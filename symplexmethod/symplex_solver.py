c60308dfa431f86e599ac8739712cfde5edbf0a6
Azat222 <azat222@gmail.com>
2024-05-11 02:24:34 +0300

    fix: fix list

--- symplexmethod/symplex_solver.py
+++ symplexmethod/symplex_solver.py
@@ -8,7 +8,7 @@ class SymplexSolver:
         self.b = b
         self.c = c
         self.n = len(a[0])
-        self.bs: list[int] = []
+        self.bs: list[int] = list()
 
     def solve(self) -> None:
         self.print_p()
