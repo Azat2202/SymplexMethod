f81931631ec1c12f0dd0d3e3244a12c984383b42
Azat222 <azat222@gmail.com>
2024-05-11 02:09:56 +0300

    feat: add builder, formatter, linter

--- symplexmethod/symplex_solver.py
+++ symplexmethod/symplex_solver.py
@@ -1,17 +1,14 @@
 from fractions import Fraction
 from typing import List
 
-from prettytable import PrettyTable
 
-
-# def __repr__(self):
 class SymplexSolver:
     def __init__(self, a: List[List[Fraction]], b: List[Fraction], c: List[Fraction]):
         self.a = a
         self.b = b
         self.c = c
         self.n = len(a[0])
-        self.bs: list[int] = list()
+        self.bs: list[int] = []
 
     def solve(self) -> None:
         self.print_p()
@@ -22,9 +19,3 @@ class SymplexSolver:
 
     def __get_p(self, i: int) -> List[Fraction]:
         return [arr[i] for arr in self.a]
-
-# @staticmethod
-# def print_symplex_table(bs: list[int], c: list[int], a: list[list[int]], result: list[int]) -> str:
-#     table = PrettyTable()
-#     table.title = ["BS", "C"] + [f"P{i}" for i in range(len(a))]
-#     return table.__repr__()
